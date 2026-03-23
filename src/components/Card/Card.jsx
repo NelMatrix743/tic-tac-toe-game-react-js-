0a40935e51e259b8a12c5adc7ab29d0101e01543
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-23 18:02:08 +0000

    feat(ui): implement card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import { useState } from 'react';
 import "./Card.css";
+
+import OPlayIcon from "~/assets/gameAssets/o_play_icon.png";
+import XPlayIcon from "~/assets/gameAssets/x_play_icon.png";
+
+
+const cardOptions = [
+  {
+    cardIcon : XPlayIcon,
+    cardClass : "card-flipped-x"
+  },
+  {
+    cardIcon : OPlayIcon,
+    cardClass : "card-flipped-o"
+  }
+];
 
 
 function Card() {
+
+  const [ flipped, setFlipped ] = useState(false);
+  const [ cardChoice, setCardChoice ] = useState({});
+
+
+  function flippedStateCallBack(){
+    if(flipped)
+      return undefined;
+    setCardChoice(cardOptions[Math.floor(Math.random() * cardOptions.length)])
+    console.log(cardChoice);
+    setFlipped((prevState) => !prevState);
+  }
+
   return (
-    <div>Card</div>
+    <div className={ flipped ? cardChoice.cardClass : "card" } onClick={ flippedStateCallBack }>
+      <img className="game-icon" src={ flipped ? cardChoice.cardIcon : null } />
+    </div>
   )
 }
 
