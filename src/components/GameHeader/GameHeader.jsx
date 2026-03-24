e292282a20baf319bdac9d21c2c7e0abbe980530
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-24 23:51:53 +0000

    add the title elements in a single div for a unified font-family

--- src/components/GameHeader/GameHeader.jsx
+++ src/components/GameHeader/GameHeader.jsx
@@ -3,15 +3,19 @@ import "./GameHeader.css";
 
 function GameHeader() {
   return (
-    <>
-    <h1 className="title">
-        <span className="red">TIC</span>
-        -
-        <span className="yellow">TAC</span>
-        -
-        <span className="orange">TOE</span>
-    </h1>
-    </>
+    <div>
+        <h1 className="title">
+            <span className="red">TIC</span>
+            -
+            <span className="yellow">TAC</span>
+            -
+            <span className="orange">TOE</span>
+        </h1>
+        <div className="sub-title">
+            <p>GAME STATUS: <span>ONGOING</span></p>
+            <p>WINNER: <span>X</span></p>
+        </div>
+    </div>
   )
 }
 
