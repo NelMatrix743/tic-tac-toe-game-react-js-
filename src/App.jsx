bc191e2aca4ec7e83b7b56ae05d130808080b6fa
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-25 22:07:13 +0000

    core structure for main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { GameHeader, CardContainer, ResetButton } from "~/components";
 
 function App() {
   return (
-    <div>App</div>
+    <div className="app">
+      <GameHeader />
+      <CardContainer />
+      <div className="center-btn">
+        <ResetButton />
+      </div>
+    </div>
   )
 }
 
